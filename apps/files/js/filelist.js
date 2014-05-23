68b9cc3c367730eeb408d4ec37970b933bbb4360
Vincent Petry <pvince81@owncloud.com>
2014-05-23 19:02:50 +0200

    Breadcrumb width calculation fix
    Rewrote the breadcrumb calculation to be more readable.
    
    Breadcrumb now has a setMaxWidth() method to set the maximum allowed
    width which is used to fit the breadcrumbs.
    
    The breadcrumb width is now based on the container width, passed through
    setMaxWidth() by the FileList class.

--- apps/files/js/filelist.js
+++ apps/files/js/filelist.js
@@ -150,11 +150,10 @@
 
 			this.$el.find('thead th .columntitle').click(_.bind(this._onClickHeader, this));
 
-			$(window).resize(function() {
-				// TODO: debounce this ?
-				var width = $(this).width();
-				self.breadcrumb.resize(width, false);
-			});
+			this._onResize = _.bind(this._onResize, this);
+			$(window).resize(this._onResize);
+
+			this.$el.on('show', this._onResize);
 
 			this.$fileList.on('click','td.filename>a.name', _.bind(this._onClickFile, this));
 			this.$fileList.on('change', 'td.filename>input:checkbox', _.bind(this._onClickFileCheckbox, this));
@@ -174,6 +173,19 @@
 				this.fileActions = new OCA.Files.FileActions();
 				this.fileActions.registerDefaultActions();
 			}
+		},
+
+		/**
+		 * Event handler for when the window size changed
+		 */
+		_onResize: function() {
+			var containerWidth = this.$el.width();
+			var actionsWidth = 64; // some padding
+			$.each(this.$el.find('#controls .actions'), function(index, action) {
+				actionsWidth += $(action).outerWidth();
+			});
+
+			this.breadcrumb.setMaxWidth(containerWidth - actionsWidth - 64);
 		},
 
 		/**
